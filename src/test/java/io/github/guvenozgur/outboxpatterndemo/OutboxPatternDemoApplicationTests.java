3d8d55f6b9f646a2e726ce69a72a9b43c907128e
Güven <gwenozgur@gmail.com>
2024-08-11 16:33:00 +0300

    init base project

---
+++ src/test/java/io/github/guvenozgur/outboxpatterndemo/OutboxPatternDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guvenozgur.outboxpatterndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutboxPatternDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
